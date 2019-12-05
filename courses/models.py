88d6a95186a4df7dfbac9133da8fc13cea25f85d
enrisan <easanchez@gmail.com>
2019-12-05 14:29:13 -0600

    Se agregó url 'courses/' en academia.urls

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 class Course(models.Model):
+    """Clase que describe los registros en la tabla de cursos Course."""
     created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=180)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+#class ClassName(object):
+#
+#    def __init__(self, arg):
+#        super(, self).__init__()
+#        self.arg = arg
--- courses/urls.py
+++ courses/urls.py
@@ -1,4 +1,4 @@
-"""academia URL Configuration
+"""courses URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('courses/', include('courses.urls')),
-    path('admin/', admin.site.urls),
-    path('', views.hello_world),
+    path('', views.courses), 
 ]
