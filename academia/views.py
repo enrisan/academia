88f889cf98d04e06de6d0328612b221a9ef5255e
enrisan <easanchez@gmail.com>
2019-12-05 16:25:41 -0600

    Agregado template para render de página de inicio.

--- academia/views.py
+++ academia/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse('Hola ORM Django')
+    return render(request, 'home.html')
